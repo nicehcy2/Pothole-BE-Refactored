1214d7f909156a3861e5006cf2c58cadbccfbcb3
nicehcy <nicehcy2@naver.com>
2025-03-10 19:23:48 +0900

    init

---
+++ src/main/java/rootcode/pothole_detection_refactored/PotholeDetectionRefactoredApplication.java
@@ -0,0 +1,13 @@
+package rootcode.pothole_detection_refactored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotholeDetectionRefactoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotholeDetectionRefactoredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rootcode/pothole_detection_refactored/PotholeDetectionRefactoredApplicationTests.java
@@ -0,0 +1,13 @@
+package rootcode.pothole_detection_refactored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotholeDetectionRefactoredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
